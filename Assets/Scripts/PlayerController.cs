6679474202a6f78c13a9c3702ed44c691a475204
unknown <nitaymaz@gmail.com>
2024-02-25 01:09:43 +0200

    added a bit of movement and animations

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float moveSpeed = 5f;
+    public Animator playerAnimator;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKey(KeyCode.W))
+        {
+            playerAnimator.SetBool("movingUp", true);
+            transform.position += Vector3.up*moveSpeed*Time.deltaTime;
+        }
+        if(Input.GetKeyUp(KeyCode.W))
+        {
+            playerAnimator.SetBool("movingUp", false);
+        }
+        if(Input.GetKey(KeyCode.S))
+        {
+            playerAnimator.SetBool("movingDown", true);
+            transform.position += Vector3.down*moveSpeed*Time.deltaTime;
+        }
+        if(Input.GetKeyUp(KeyCode.S))
+        {
+            playerAnimator.SetBool("movingDown", false);
+        }
+        if(Input.GetKey(KeyCode.A))
+        {
+            transform.position += Vector3.left*moveSpeed*Time.deltaTime;
+        }
+        if(Input.GetKey(KeyCode.D))
+        {
+            transform.position += Vector3.right*moveSpeed*Time.deltaTime;
+        }
+    }
+}
